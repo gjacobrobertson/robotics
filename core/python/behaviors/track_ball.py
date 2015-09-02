77cbd291ba2cd266988025aa0c8bf5d5232c0275
Jacob Robertson <jacobr@cs.utexas.edu>
2015-09-01 19:08:42 -0500

    ball tracking

--- core/python/behaviors/track_ball.py
+++ core/python/behaviors/track_ball.py
@@ -6,79 +6,26 @@ from state_machine import *
 import core,util
 from memory import *
 
-
 DEG_T_RAD = core.DEG_T_RAD
+RAD_T_DEG = core.RAD_T_DEG
 
-class Ready(Task):
+class Playing(Task):
   def run(self):
-    commands.standStraight()
-    if self.getTime() > 5.0:
-      memory.speech.say("ready to play")
-      self.finish()
-
-class Set(Task):
-  def run(self):
-    commands.standStraight()
-
-class Playing(StateMachine):
-  class Stand(Node):
-    def run(self):
-      print "Stand"
-      commands.stand()
-      if self.getTime() > 3.0:
-        self.finish()
-
-  class BallLostEvent(Event):
-    def __init__(self):
-      super(self.__class__, self).__init__()
-      self.lostCount = 0;      
-
-    def ready(self):
-      ball = world_objects.getObjPtr(core.WO_BALL)
-#      print "LostEvent {0} to {1}".format(ball.frameLastSeen, util.currentFrame())
-      if not ball.seen: #util.currentFrame() - ball.frameLastSeen > 30:
-	print "lost"
-        self.lostCount += 1
+    commands.setStiffness()
+    ball = world_objects.getObjPtr(core.WO_BALL)
+    currentTilt = core.joint_values[core.HeadTilt] * RAD_T_DEG
+    trackingSpeed = 10
+    if ball.seen:
+      aboveCenter = ball.imageCenterY < 100
+      belowCenter = ball.imageCenterY > 140
+      print "Current Tilt: {0}".format(currentTilt)
+      print "AboveCenter: {}".format(aboveCenter)
+      print "BelowCenter: {0}".format(belowCenter)
+      if aboveCenter and currentTilt < 30:
+        commands.setHeadTilt(currentTilt + trackingSpeed)
+      elif belowCenter and currentTilt > -30:
+        commands.setHeadTilt(currentTilt - trackingSpeed)
       else:
-        self.lostCount -= 1
-      if self.lostCount > 5:
-        return True
-      else:
-        return False
-
-  class BallFoundEvent(Event):
-    def __init__(self):
-      super(self.__class__,self).__init__()
-      self.foundCount = 0
-
-    def ready(self):
-      ball = world_objects.getObjPtr(core.WO_BALL)
-      if ball.seen:
-        print "find"
-        self.foundCount += 1
-      else:
-        self.foundCount -= 1
-      if self.foundCount > 5:
-        return True
-      else:
-        return False
-
-  class TrackBall(Node):
-    def run(self):
-      return head.TrackBall()  
-  
-  class BallSearch(Node):
-    def run(self):
-      return head.ScanForBall()    
-
-  def setup(self):
-    stand = self.Stand()
-    lost = self.BallLostEvent()
-    found = self.BallFoundEvent()
-    track = self.TrackBall()
-    search = self.BallSearch()
-
-#    self.trans(stand, T(20.0), stand)
-    self.trans(search, found, track)
-    self.trans(track, lost, search)
-    
+        commands.setHeadTilt(currentTilt)
+    else:
+        commands.setHeadTilt(currentTilt)
--- core/python/head.py
+++ core/python/head.py
@@ -5,9 +5,6 @@ import core
 import cfgstiff
 from memory import *
 
-DEG_T_RAD = core.DEG_T_RAD
-RAD_T_DEG = core.RAD_T_DEG
-
 class MoveHead(Task):
   def __init__(self, tilt = None, pan = 0.0, time = 2.0):
     Task.__init__(self)
@@ -22,36 +19,3 @@ class MoveHead(Task):
 
     if self.getTime() > self.time: self.finish()
 
-class TrackBall(Task):
-  def __init__(self):
-    Task.__init__(self)
-    
-  def run(self):
-    ball = world_objects.getObjPtr(core.WO_BALL)
-    angle = ball.visionBearing
-#    commands.stand()
-    print "Tracking ball: {0}".format(angle)
-    print "In Image: {0}, {1}".format(ball.imageCenterX, ball.imageCenterY)
-    commands.setHeadPanTilt(angle, -24, 0.2)
-
-class ScanForBall(Task):
-  def __init__(self):
-    Task.__init__(self)
-    self.seenBallCount = 0
-    self.ballCountThreshold = 0;
-    self.sign = 1
-    print "Init Scan"
-
-  def run(self):
-    print "Scan!"    
-    if core.joint_values[core.HeadYaw] >= 40 * DEG_T_RAD:
-      self.sign *= -1
-      self.resetTime()
-    elif core.joint_values[core.HeadYaw] <= -40 * DEG_T_RAD:
-      self.sign *= -1
-      self.resetTime()
-
-#    commands.stand()
-    print "Angle: {0}, Sign: {1}".format(core.joint_values[core.HeadYaw] * RAD_T_DEG,self.sign)
-    commands.setStiffness()
-    commands.setHeadPan(self.sign * 45 * DEG_T_RAD, 2.0)
