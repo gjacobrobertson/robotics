b18ff76c821f0420295f540e90120bcb13495a83
Jacob Robertson <jacobr@cs.utexas.edu>
2015-09-14 13:46:20 -0500

    cleaned up blob detector

--- core/vision/BlobDetector.cpp
+++ core/vision/BlobDetector.cpp
@@ -3,15 +3,13 @@
 #include "structures/Blob.h"
 
 BlobDetector::BlobDetector(DETECTOR_DECLARE_ARGS) : DETECTOR_INITIALIZE {
-  dx = 1;
-  dy = 1;
+  dx = 4;
+  dy = 2;
   width = 5;
   height = 5;
 }
 
 vector<Blob*> BlobDetector::findBlobs(unsigned char* segImg) {
-//  if(camera_ == Camera::BOTTOM) return;
-//  cout << "Find Blobs" << endl;
   vector<vector<Node*>> rows = findRuns(segImg);
   
   mergeRuns(rows);
@@ -21,28 +19,21 @@ vector<Blob*> BlobDetector::findBlobs(unsigned char* segImg) {
 
   for (int i=0; i<rows.size(); i++){
     for (int j=0; j<rows[i].size(); j++) {
-//      if (rows[i][j]->data->color == c_ORANGE){
-        cout <<"Row: " << i << " ";
-        rows[i][j]->print();
-//      }
-      if (rows[i][j]->parent == rows[i][j]){  // Points to self
-        blob = new Blob();
-        blob->xi = rows[i][j]->data->xi;
-        blob->xf = rows[i][j]->data->xf;
-        blob->yi = rows[i][j]->data->yi;
-        blob->yf = rows[i][j]->data->yf;
+      Node *node = rows[i][j];
+      if (node->parent == node){  // Points to self
+        Blob *blob = new Blob();
+        blob->xi = node->data->xi;
+        blob->xf = node->data->xf;
+        blob->yi = node->data->yi;
+        blob->yf = node->data->yf;
         blob->dx = (blob->xf - blob->xi) + 1;
         blob->dy = (blob->yf - blob->yi) + 1;
-        blob->color = rows[i][j]->data->color;
-        blob->avgWidth = (rows[i][j]->data->color_ct) * (dy*1.0) / blob->dy;
+        blob->color = node->data->color;
+        blob->avgWidth = (node->data->color_ct) * (dy*1.0) / blob->dy;
         blob_list.push_back(blob);
-//        cout << "BlobDetet: " << (int)blob->color << endl;
       }
     }
   }
-
-  for (int i=0; i<blob_list.size(); i++)
-    blob_list[i]->print();
 
   for (int i=0; i < rows.size(); i++) {
     for (int j=0; j < rows[i].size(); j++){
@@ -51,24 +42,18 @@ vector<Blob*> BlobDetector::findBlobs(unsigned char* segImg) {
     rows[i].clear();
   }
   rows.clear();
-//  for (int i=0; i<blob_list.size(); i++)
-//    delete blob_list[i];
   return blob_list;
 }
 
 vector<vector<Node*>> BlobDetector::findRuns(unsigned char* segImg) {
   vector<vector<Node*>> rows;
-  //unsigned char* segImg = vblocks_.image->getImgTop();
-  unsigned char testImg[25] =   {1,1,3,1,1,
-				 0,1,0,1,0,
-				 0,1,1,1,0,
-				 2,1,1,1,2,
-				 2,1,1,1,0};
-//  int dy = 10;
+//  unsigned char testImg[25] =   {1,1,3,1,1,
+//				 0,1,0,1,0,
+//				 0,1,1,1,0,
+//				 2,1,1,1,2,
+//				 2,1,1,1,0};
   for (int y=0; y < height; y+=dy) {
-    //cout << y << endl;
-    //cout << "SegIMG: " << y<< " " <<(int) segImg[y * iparams_.width] << endl;
-    rows.push_back(findRunsInRow(testImg, y));
+    rows.push_back(findRunsInRow(segImg, y));
   }
   return rows;
 }
@@ -99,7 +84,6 @@ vector<Node*> BlobDetector::findRunsInRow(unsigned char *segImg, int y) {
 
 void BlobDetector::mergeRuns(vector<vector<Node*>> &rows) {
   for (int y=dy; y<height;y+=dy) {
-    //cout << "Y: " << y << endl;
     vector<Node*> firstRow = rows[y/dy - 1];
     vector<Node*> secondRow = rows[y/dy];
     int i=0;
