794a0199935e0163f577bc0490dafacff8e9ed5f
Jacob Robertson <jacobr@cs.utexas.edu>
2015-09-24 17:57:25 -0500

    basic pid approach

---
+++ core/python/pid.py
@@ -0,0 +1,20 @@
+class PID(object):
+  def __init__(self, kp, ki, kd):
+    self.kp = kp
+    self.ki = ki
+    self.kd = kd
+    self.last_error = 0
+    self.cum_error = 0
+
+  def update(e):
+    p = self.kp * e
+
+    #Update integral
+    self.cum_error += e
+    i = self.ki * self.cum_error
+
+    #Update Derivative
+    d = self.kd * (e - self.last_error)
+    self.last_error = e
+ 
+    return p + i + d
